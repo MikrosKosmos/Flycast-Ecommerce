4ed0c93b47c39ed17522efb6caf40a862a54f7fd
Abbas Ali <abbasatrix@outlook.com>
2020-10-07 16:47:35 +0530

    Changes in SignIn and SignUp form

--- UI/Website/src/app/views/base/index/login/login.component.ts
+++ UI/Website/src/app/views/base/index/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +8,29 @@ import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  isLogin: boolean;
+
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
       username: ['', Validators.required],
-      password: ['', Validators.required]
-  });
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+    this.isLogin = false;
   }
 
-  formLogin():void {
+  formLogin(): void {
     console.log("hello");
+  }
+
+  changeFormType(user) {
+    console.log(user);
+    user.active = !user.active
   }
 }
