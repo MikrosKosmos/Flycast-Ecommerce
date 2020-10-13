f4418fabed74f68e95c26200627ce1c5e77d1ec1
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-10-13 17:22:55 +0530

    Added API logger and Token Check SPs.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

---
+++ Backend/Microservices/UserService/DB/StoredProcedures/sp_LogApiStatus.sql
@@ -0,0 +1,8 @@
+DROP PROCEDURE IF EXISTS sp_LogApiStatus;
+CREATE PROCEDURE sp_LogApiStatus(in requestKey varchar(50), in parPath varchar(50), in apiStatus INT,
+                                 in responseCode INT,
+                                 in apiKey varchar(200))
+BEGIN
+    INSERT INTO tbl_ApiLogger(request_Key, api_token, path, response_code, api_status, created_by)
+    VALUES (requestKey, apiKey, parPath, responseCode, apiStatus, 1);
+end;
