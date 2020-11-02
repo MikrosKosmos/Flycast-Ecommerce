fdde9002591a6b32672ad59e4605b1ff3bfb6a0a
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-11-02 19:57:10 +0530

    Updated SP to Assign product to an Order.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- Backend/Microservices/OrderInventoryService/DB/StoredProcedures/sp_AssignProductToOrder.sql
+++ Backend/Microservices/OrderInventoryService/DB/StoredProcedures/sp_AssignProductToOrder.sql
@@ -1,10 +1,33 @@
 drop procedure if exists sp_AssignProductToOrder;
 create procedure sp_AssignProductToOrder(parOrderId int)
 begin
-    set @isValid = 0;
-    select id into @isValid from tbl_OrderMaster where id = parOrderId and is_active = 1;
+    declare i int default 0;
+    declare j int default 0;
+    set @isValid = 1;
+    #select id into @isValid from tbl_OrderMaster where id = parOrderId and is_active = 1;
     if @isValid > 0 then
-
+        set @noOfProducts = 0;
+        select count(id) into @noOfProducts from tbl_OrderProductDetails where order_id = parOrderId and is_active = 1;
+        while i < @noOfProducts
+            do
+                set @quantity = 0;
+                set @orderDetailsId = 0;
+                set @skuVal = '';
+                select quantity, id, sku
+                into @quantity, @orderDetailsId,@skuVal
+                from tbl_OrderProductDetails
+                where order_id = parOrderId
+                  and is_active = 1
+                limit i,1;
+                set i = i + 1;
+                set j = 0;
+                while j < @quantity
+                    do
+                        insert into tbl_OrderAssetDetails(order_details_id, asset_id, created_by)
+                            value (@orderDetailsId, 0, 1);
+                        set j = j + 1;
+                    end while;
+            end while;
     else
         select -1 as id;
     end if;
