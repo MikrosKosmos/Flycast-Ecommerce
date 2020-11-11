4dc5ddd6db24fa06117c52fe8f2c5a5cf379f56c
KBHELLO <banerjee4785@gmail.com>
2020-11-11 22:04:29 +0530

    navbar and login updated

--- UI/Website/src/app/views/base/index/navbar/navbar.component.ts
+++ UI/Website/src/app/views/base/index/navbar/navbar.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   isRegistered: boolean;
   userName: string;
   isCollapsed = true;
   isCollapsed2 = true;
-  constructor(private router: Router) { }
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+
+  constructor(
+    private router: Router,
+    public location: Location,
+    private element: ElementRef
+  ) {
+    this.sidebarVisible = false;
+  }
 
   @ViewChild(LoginComponent) loginComponent: LoginComponent;
 
   ngOnInit(): void {
     this.userName = sessionStorage.getItem('FirstName');
-    //console.log('firstname', this.userName, sessionStorage.getItem('FirstName'));
-    if (this.userName != null)
-      this.isRegistered = true;
-    else
-      this.isRegistered = false;
+    console.log('username ', this.userName);
+    if (this.userName != null) this.isRegistered = true;
+    else this.isRegistered = false;
   }
   logout() {
     //console.log('clear the session');
