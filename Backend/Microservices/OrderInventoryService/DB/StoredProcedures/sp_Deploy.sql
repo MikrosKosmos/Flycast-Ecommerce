4ce018ae330996e4e3a0ffb7b8f663629cec4fe2
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2021-01-04 11:10:02 +0530

    Added SP to truncate tables for order-inventory service.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

---
+++ Backend/Microservices/OrderInventoryService/DB/StoredProcedures/sp_Deploy.sql
@@ -0,0 +1,18 @@
+drop procedure if exists sp_Deploy;
+create procedure sp_Deploy()
+begin
+    truncate tbl_CategoryMaster;
+    truncate tbl_SkuMaster;
+    truncate tbl_SkuPictures;
+    truncate tbl_SkuRatingDetails;
+    truncate tbl_AssetMaster;
+    truncate tbl_AssetAttributeValue;
+    truncate tbl_InventoryMaster;
+    truncate tbl_OrderMaster;
+    truncate tbl_OrderAssetDetails;
+    truncate tbl_OrderProductDetails;
+    truncate tbl_ApiLogger;
+    truncate tbl_CartMaster;
+    truncate tbl_CartDetails;
+    select 1 as id;
+end;
